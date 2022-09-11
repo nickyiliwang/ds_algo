91d0cee459c969d7d99817c7ad6d352233a49c85
nickyiliwang <nickyiliwang@gmail.com>
2022-09-11 13:22:45 -0400

    WIP valid-sudoku

---
+++ 7.valid-sudoku.py
@@ -0,0 +1,78 @@
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+
+
+# Example 1:
+# Input: board =
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+
+# Example 2:
+# Input: board =
+# [["8","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+
+# Constraints:
+
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit 1-9 or '.'.
+
+# Trick:
+# Each sub block can be split into large blocks
+# To find out which sub block, we can just do (row/3 , and col/3)
+# using HashDictionary (position of block) and HashSets (unique numbers)
+
+#   0 - 9
+# 0 0 1 2
+# | 1
+# 9 2
+
+import collections
+
+
+def isValidSudoku(board):
+    validator = collections.defaultdict(set)
+    res = True
+
+    for colIdx, col in enumerate(board):
+        print(colIdx)
+        for rowIdx, num in enumerate(col):
+            if (validator[tuple(colIdx//3, rowIdx // 3)]):
+                print("no dups")
+
+    print(validator)
+    print(res)
+    return res
+
+
+board = [["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8", ".",
+                                                                                                                                                                                                      "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+# Output: true
+
+isValidSudoku(board)
