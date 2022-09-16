247351131ff3a8dd786076519ea27fce4a027653
nickyiliwang <nickyiliwang@gmail.com>
2022-09-16 04:40:17 -0400

    3sum WIP

--- 11.two-sum-input-array-sorted.py
+++ 11.two-sum-input-array-sorted.py
@@ -31,6 +31,8 @@
 # -1000 <= target <= 1000
 # The tests are generated such that there is exactly one solution.
 
+
+# constant extra space O(1)
 # using pointers
 # because of sorted array we can do this
 # if current sum is bigger than target, move the right pointer to the left to decrease the sum
@@ -49,7 +51,8 @@ def twoSum(numbers, target):
             l += 1
 
         else:
-            return [l, r]
+            # added by one as an integer array [index1, index2] of length 2
+            return [l + 1, r + 1]
 
 
 numbers = [2, 7, 11, 15]
