d5e368e6835cb7cf8071bf2bd4df8f697e813141
nickyiliwang <nickyiliwang@gmail.com>
2023-01-10 05:04:33 -0500

    sliding window, dsalgo problem solving framwork

--- 20.sliding-window-maximum.py
+++ 20.sliding-window-maximum.py
@@ -1,7 +1,9 @@
+from collections import deque
+from typing import List
+
 # You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 
 # Return the max sliding window.
-
 
 # Example 1:
 
@@ -21,9 +23,45 @@
 # Input: nums = [1], k = 1
 # Output: [1]
 
-
 # Constraints:
 
 # 1 <= nums.length <= 105
 # -104 <= nums[i] <= 104
 # 1 <= k <= nums.length
+
+
+def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
+    res = []
+    left = right = 0
+    # deque can popleft the beginning and add/remove at the end in constant time
+    q = deque()
+
+    # stop right before length of array, len is always index + 1
+    while right < len(nums):
+        # monotonic decreasing queue
+        # pops all numbers at the end of the queue smaller than right pointer number
+        while q and nums[q[-1]] < nums[right]:
+            q.pop()
+
+        # appending right index
+        q.append(right)
+
+        # if left pointer is bigger than the beginning index of deque
+        # meaning we want to popleft the left val from the window
+        if left > q[0]:
+            q.popleft()
+
+        # if we arrive at the sliding window
+        # append the left most val to result
+        # increment left pointer
+        if (right - left + 1) == k:
+            res.append(nums[q[0]])
+            left += 1
+
+        right += 1
+
+    print(res)
+    return res
+
+
+maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3)
--- 8.encode-decode-strings.py
+++ 8.encode-decode-strings.py
@@ -12,18 +12,21 @@
 
 # Space: O(n) Time: O(n)
 
+
 def encode(strs):
     res = ''
     for word in strs:
         res = res + str(len(word)) + "#" + word  # "#" is the delimiter
     return res
 
+
 # neetcode
 
 
 def decode(str):
+    res = []
     # i variable is the character index
-    res, i = [], 0
+    i = 0
 
     # before we reach the length of the string
     while i < len(str):
@@ -33,21 +36,23 @@ def decode(str):
         while str[j] != "#":
             j += 1
         # length variable can get double or triple digit word lengths
+        # str[start:stop] <= does not include stop
+        # that's why we did j += 1 in above 
         length = int(str[i:j])
         print("length", length)
         # we do j + 1 becasue we are skipping the "#" delimiter
         # the word is start: j + 1, and end: j + 1 + length
-        res.append(str[j + 1: j + 1 + length])
+        res.append(str[j + 1:j + 1 + length])
         # like my own solution, char index will restart at last end
         i = j + 1 + length
     print(res)
     return res
 
+
 # # Space: O(n) Time: O(n)
 # # My solution with while loop
 # # THIS DOES NOT WORK WHEN we have over 10 letters
 # # ie. 10#asdfghjkli4#code4#love3#you
-
 
 # def decode(str):
 #     res, i = [], 0
@@ -84,11 +89,14 @@ def decode(str):
 #             res.append(str[])
 #     return res
 
-
-strs = ["asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4", "code", "love", "you"]
+strs = [
+    "asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4asdfghjkli4",
+    "code", "love", "you"
+]
 str = encode(strs)
+print("encoded", str)
 # OUTPUT single string
 decode(str)
 # OUTPUT ["", "we", "#say", ":", "yes"]
 
-# watching event horizon while trying to debug my code is not the best way to focus 
+# watching event horizon while trying to debug my code is not the best way to focus
