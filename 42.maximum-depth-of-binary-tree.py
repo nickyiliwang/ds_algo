caf56bcec4cb4cab843063559bc9f87170ee53ec
nickyiliwang <yw12me@gmail.com>
2023-02-08 07:23:58 -0500

    reviewing trees

--- 42.maximum-depth-of-binary-tree.py
+++ 42.maximum-depth-of-binary-tree.py
@@ -1,36 +1,11 @@
 from typing import Optional
-from ds_types.tree import Tree
+from ds_types.tree import Tree, TreeNode
 from collections import deque
-
-# Given the root of a binary tree, return its maximum depth.
-
-# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
-
-# Example 1:
-
-# Input: root = [3,9,20,null,null,15,7]
-# Output: 3
-# Example 2:
-
-# Input: root = [1,null,2]
-# Output: 2
-
-# Constraints:
-
-# The number of nodes in the tree is in the range [0, 104].
-# -100 <= Node.val <= 100
-
-
-# Definition for a binary tree node.
-class TreeNode:
-
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 
 # recursive DFS
 # T: O(n), M: O(n)
+
+
 def maxDepth(root: Optional[TreeNode]) -> int:
     if not root:
         return 0
@@ -38,19 +13,19 @@ def maxDepth(root: Optional[TreeNode]) -> int:
     return 1 + max(maxDepth(root.left), maxDepth(root.right))
 
 
-# # iterative DFS (pre-ordered)
-# def maxDepth(root: Optional[TreeNode]) -> int:
-#     stack = [[root, 1]]
-#     res = 0
-#     while stack:
-#         node, depth = stack.pop()
+# iterative DFS (pre-ordered)
+def maxDepth(root: Optional[TreeNode]) -> int:
+    stack = [[root, 1]]
+    res = 0
+    while stack:
+        node, depth = stack.pop()
 
-#         if node:
-#             res = max(res, depth)
-#             stack.append([node.left, depth + 1])
-#             stack.append([node.right, depth + 1])
+        if node:
+            res = max(res, depth)
+            stack.append([node.left, depth + 1])
+            stack.append([node.right, depth + 1])
 
-#     return res
+    return res
 
 # # iterative BFS (queue)
 # # queue is FIFO
