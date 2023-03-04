4c4c3a59a99e4cd63407b8c7cb82f98f61416ca3
nickyiliwang <nickyiliwang@gmail.com>
2023-03-04 18:37:11 -0500

    baby steps, training season back in

---
+++ 0Arrays&Hashing/1.replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,11 @@
+from typing import *
+import os
+os.system('clear')
+
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        print('hey!')
+
+
+Solution.replaceElements(1, [])
