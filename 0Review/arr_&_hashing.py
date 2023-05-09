e8d914943e944e2a76bb0afb622599f0e5fcc12b
nickyiliwang <nickyiliwang@gmail.com>
2023-05-09 02:09:34 -0400

    review day 0: haha this isn't going so well

---
+++ 0Review/arr_&_hashing.py
@@ -0,0 +1,20 @@
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+
+def two_sum(arr, tar):
+    hash = {}
+    res = []
+    for n in arr:
+        if():
+            return 
+hash[n] = tar - n
+    print()
+
+
+two_sum([2, 7, 11, 15], 9)
+
+
+hash.get(n) is not None
