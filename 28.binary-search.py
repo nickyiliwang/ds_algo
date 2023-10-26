22fbd71ef6fb25269189ff9a802c2773791edd48
nickyiliwang <nickyiliwang@gmail.com>
2023-10-26 08:37:49 -0400

    median of 2 sorted arrays, heheheheahahahahaouhfdo\ubnsdv;ibdv;isl dubns;dfue;hfeohfw;efjw;

--- 28.binary-search.py
+++ 28.binary-search.py
@@ -1,4 +1,4 @@
-def search(self, nums: List[int], target: int) -> int: 
+def search(self, nums, target: int) -> int:
     l, r = 0, len(nums) - 1
     while l <= r:
         m = (l + r) // 2
@@ -10,35 +10,4 @@ def search(self, nums: List[int], target: int) -> int:
             return m
     return -1
 
-def search(nums, target):
-    # default return -1
-    res = -1
-    # left and right pointers
-    left = 0
-    right = len(nums) - 1
-
-    # while left is before right
-    # or if they are equal, meaning either the number of items are 1
-    while left <= right:
-        # get the mid pointer
-        # (l + r) // 2 can lead to overflow
-        middle = left + ((right - left) // 2)
-        # return the index of correct number
-        if (nums[middle] == target):
-            res = middle
-        # if the number is smaller than the target
-        # we ove left pointer up
-        if (nums[middle] < target):
-            left = middle + 1
-        # number is too large
-        # move the right pointer down
-        else:
-            right = middle - 1
-
-    return res
-
-
-nums = [1,2,3,4,5]
-target = 5
-
-search(nums, target)
+print(search())
--- 34.median-of-two-sorted-arrays.py
+++ 34.median-of-two-sorted-arrays.py
@@ -1,83 +1,97 @@
-def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
-    A, B = nums1, nums2
-    total = len(nums1) + len(nums2)
-    half = total // 2 - 2
-
-    if len(A) > len(B):
-        A, B = nums2, nums1
-
-    left, right = 0, len(A) - 1
-    while True:
-        A_Middle = (left + right) // 2
-        B_Middle = half - A_Middle
-
-        A_Left = A[A_Middle] if A_Middle >= 0 else float("-infinity")
-        A_Right = A[A_Middle + 1] if A_Middle < len(A) - 1 else float("infinity")
-
-        B_Left = B[B_Middle] if B_Middle >= 0 else float("-infinity")
-        B_Right = B[B_Middle + 1] if B_Middle < len(B) - 1 else float("infinity")
-
-        if A_Left <= B_Right and B_Left <= A_Right:
-            if total % 2 == 0:
-                return (max(A_Left, B_Left) + min(A_Right, B_Right)) / 2
-            return min(A_Right, B_Right)
-        elif A_Left > B_Right:
-            right = A_Middle - 1
-        else:
-            left = A_Middle + 1
+from typing import *
 
 
-# O(log(m + n))
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2 - 2
+
+        if len(A) > len(B):
+            A, B = nums2, nums1
+
+        left, right = 0, len(A) - 1
+        while True:
+            A_Mid = (left + right) // 2
+            B_Mid = half - A_Mid
+
+            A_Left = A[A_Mid] if A_Mid >= 0 else float("-inf")
+            A_Right = A[A_Mid + 1] if A_Mid < len(A) - 1 else float("inf")
+
+            B_Left = B[B_Mid] if B_Mid >= 0 else float("-inf")
+            B_Right = B[B_Mid + 1] if B_Mid < len(B) - 1 else float("inf")
+
+            if A_Left <= B_Right and B_Left <= A_Right:
+                if total % 2 == 0:
+                    return (max(A_Left, B_Left) + min(A_Right, B_Right)) / 2
+                else:
+                    return min(A_Right, B_Right)
+            elif A_Left > B_Right:
+                right = A_Mid - 1
+            else:
+                left = A_Mid + 1
+
+
+nums1 = [1, 2, 3, 4]
+nums2 = [3, 4, 4, 5, 5, 6, 7, 8]
+
+print(Solution.findMedianSortedArrays("", nums1, nums2))
+
+# Time: O(log(min(m, n)))
+# Space: O(n)
 # using partitions:
 # A: [1,2,3]
 # B: [1,2,3] [4,5,6,7,8]
-def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
-    A, B = nums1, nums2
-    total = len(nums1) + len(nums2)
-    # Since "half" is derived from length, we -2 to fix off by one
-    half = total // 2 - 2
-
-    # A will always be shorter array
-    if len(A) > len(B):
-        A, B = nums2, nums1
-
-    left, right = 0, len(A) - 1
-    while True:
-        A_Middle = (left + right) // 2
-        # getting the left partition of B
-        B_Middle = half - A_Middle
-
-        A_Left = A[A_Middle] if A_Middle >= 0 else float("-infinity")
-        A_Right = A[A_Middle + 1] if A_Middle < len(A) - 1 else float("infinity")
-
-        B_Left = B[B_Middle] if B_Middle >= 0 else float("-infinity")
-        B_Right = B[B_Middle + 1] if B_Middle < len(B) - 1 else float("infinity")
-
-        # Check partitions
-        if A_Left <= B_Right and B_Left <= A_Right:
-            # even
-            if total % 2 == 0:
-                return (max(A_Left, B_Left) + min(A_Right, B_Right)) / 2
-            # odd
-            return min(A_Right, B_Right)
-        elif A_Left > B_Right:
-            right = A_Middle - 1
-        else:
-            left = A_Middle + 1
+# Binary search to find the median
 
 
-nums1 = [1, 2]
-nums2 = [3, 4]
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2 - 2
 
-print(findMedianSortedArrays(nums1, nums2))
+        if len(A) > len(B):
+            A, B = nums2, nums1
+
+        left, right = 0, len(A) - 1
+        while True:
+            A_Mid = (left + right) // 2
+            B_Mid = half - A_Mid
+
+            # ALeft -inf [{Left can be out}Mid{Right can be out of bound}] +inf A_Right
+            A_Left = A[A_Mid] if A_Mid >= 0 else float("-inf")
+            A_Right = A[A_Mid + 1] if A_Mid < len(A) - 1 else float("inf")
+
+            B_Left = B[B_Mid] if B_Mid >= 0 else float("-inf")
+            B_Right = B[B_Mid + 1] if B_Mid < len(B) - 1 else float("inf")
+
+            if A_Left <= B_Right and B_Left <= A_Right:
+                # Even
+                if total % 2 == 0:
+                    # [{1,2,[3]},{[4]}]
+                    # [{1,2,[3]},{[4],5,6,7}]
+                    # max(3, 3) + min(4,4)
+                    return (max(A_Left, B_Left) + min(A_Right, B_Right)) / 2
+                # Odd
+                else:
+                    return min(A_Right, B_Right)
+            # [{1,2,3,4}] A_left = 4
+            # [{1,2},{3,4,5,6,7}] B_right = 3
+            #  A_left(4) cannot be bigger than B_right(3)
+            # Partition is wrong
+            elif A_Left > B_Right:
+                right = A_Mid - 1
+            else:
+                left = A_Mid + 1
 
 # Divide-and-conquer approach.
 # The function takes in two input lists nums1 and nums2 and returns a float as the median. The function first assigns the shorter of the two input lists to a variable A and the longer one to a variable B.
-# It then calculates the middle index of the combined list by taking the floor division of the total number of elements in the two lists by 2.
-# It then enters a while loop. Within the loop, it finds the middle index of both lists A and B and assigns them to A_Middle and B_Middle respectively.
-# It then checks if the element at the middle index of A is less than or equal to the element immediately following the middle index of B, and if the element at the middle index of B is less than or equal to the element immediately following the middle index of A.
-# If this is true, it checks if the total number of elements in the two lists is even or odd. If it's even, it returns the average of the largest element of the two middle elements of the two arrays and the smallest element of the two middle elements of the two arrays as the median. If it's odd, it returns the smaller element of the two middle elements of the two arrays as the median.
-# If it's not true, it checks if the element at the middle index of A is greater than the element immediately following the middle index of B. If it is, it moves the right pointer one step to the left, else it moves the left pointer one step to the right.
+# It then calculates the Mid index of the combined list by taking the floor division of the total number of elements in the two lists by 2.
+# It then enters a while loop. Within the loop, it finds the Mid index of both lists A and B and assigns them to A_Mid and B_Mid respectively.
+# It then checks if the element at the Mid index of A is less than or equal to the element immediately following the Mid index of B, and if the element at the Mid index of B is less than or equal to the element immediately following the Mid index of A.
+# If this is true, it checks if the total number of elements in the two lists is even or odd. If it's even, it returns the average of the largest element of the two Mid elements of the two arrays and the smallest element of the two Mid elements of the two arrays as the median. If it's odd, it returns the smaller element of the two Mid elements of the two arrays as the median.
+# If it's not true, it checks if the element at the Mid index of A is greater than the element immediately following the Mid index of B. If it is, it moves the right pointer one step to the left, else it moves the left pointer one step to the right.
 
 
 # def findMedianSortedArrays(nums1, nums2):
@@ -86,22 +100,22 @@ print(findMedianSortedArrays(nums1, nums2))
 #     def addSecondList(list, n):
 #         left, right = 0, len(list) - 1
 #         while left <= right:
-#             middle = (left + right) // 2
-#             if list[middle] < n:
-#                 if (middle + 1) < len(list) and list[middle + 1] > n:
-#                     list.insert(middle + 1, n)
+#             Mid = (left + right) // 2
+#             if list[Mid] < n:
+#                 if (Mid + 1) < len(list) and list[Mid + 1] > n:
+#                     list.insert(Mid + 1, n)
 #                     return
-#                 elif (middle + 1) == len(list):
-#                     list.insert(middle + 1, n)
+#                 elif (Mid + 1) == len(list):
+#                     list.insert(Mid + 1, n)
 #                     return
-#                 left = middle + 1
+#                 left = Mid + 1
 
-#             elif list[middle] == n:
-#                 list.insert(middle + 1, n)
-#                 left = middle + 1
+#             elif list[Mid] == n:
+#                 list.insert(Mid + 1, n)
+#                 left = Mid + 1
 #                 return
 #             else:
-#                 right = middle - 1
+#                 right = Mid - 1
 
 #     # compare initial list
 
@@ -130,8 +144,8 @@ print(findMedianSortedArrays(nums1, nums2))
 #         return (merged[leftPart] + merged[rightPart]) / 2
 #     else:
 #         left, right = 0, len(merged) - 1
-#         middle = (left + right) // 2
-#         return merged[middle]
+#         Mid = (left + right) // 2
+#         return merged[Mid]
 
 # nums1 = [1, 2]
 # nums2 = [3, 4]
@@ -143,3 +157,17 @@ print(findMedianSortedArrays(nums1, nums2))
 # The time complexity of this function is O(n log n) because it combines two sorted arrays by inserting elements of the second array one by one into the first array using a binary search algorithm, which has a time complexity of O(log n). This binary search operation is performed n times, once for each element in the second array, resulting in a total time complexity of O(n log n).
 
 # The space complexity of this function is O(n) because it creates a new array, merged, which has a maximum size of n, where n is the total number of elements in the two input arrays.
+
+
+# Brute Force
+# class Solution:
+#     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+#         merged = nums1 + nums2
+#         merged.sort()
+
+#         total = len(merged) - 1
+#         mid = total // 2
+#         if (total % 2 == 1):
+#             return merged[mid]
+#         else:
+#             return (merged[mid] + (merged[mid] - 1)) / 2
