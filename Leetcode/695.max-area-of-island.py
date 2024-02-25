46931050f07443e064159cbea06a4fd25fba6797
Nick Wang <yw12me@gmail.com>
2024-02-25 05:00:50 -0500

    max area of island WIP

---
+++ Leetcode/695.max-area-of-island.py
@@ -0,0 +1,113 @@
+#
+# @lc app=leetcode id=695 lang=python3
+#
+# [695] Max Area of Island
+#
+# https://leetcode.com/problems/max-area-of-island/description/
+#
+# algorithms
+# Medium (71.87%)
+# Likes:    9769
+# Dislikes: 198
+# Total Accepted:    822.5K
+# Total Submissions: 1.1M
+# Testcase Example:  '[[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]'
+#
+# You are given an m x n binary matrix grid. An island is a group of 1's
+# (representing land) connected 4-directionally (horizontal or vertical.) You
+# may assume all four edges of the grid are surrounded by water.
+#
+# The area of an island is the number of cells with a value 1 in the island.
+#
+# Return the maximum area of an island in grid. If there is no island, return
+# 0.
+#
+#
+# Example 1:
+#
+#
+# Input: grid =
+# [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+# Output: 6
+# Explanation: The answer is not 11, because the island must be connected
+# 4-directionally.
+#
+#
+# Example 2:
+#
+#
+# Input: grid = [[0,0,0,0,0,0,0,0]]
+# Output: 0
+#
+#
+#
+# Constraints:
+#
+#
+# m == grid.length
+# n == grid[i].length
+# 1 <= m, n <= 50
+# grid[i][j] is either 0 or 1.
+#
+#
+#
+
+from typing import List
+
+
+# 518/728 cases passed (N/A)
+# @lc code=start
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        row, col = len(grid), len(grid[0])
+        islands = 0
+        visited = set()
+        maxArea = float("-inf")
+
+        def dfs(r, c, area):
+            if (
+                r not in range(row)
+                or c not in range(col)
+                or (r, c) in visited
+                or grid[r][c] == 0
+            ):
+                return area
+
+            visited.add((r, c))
+            return max(
+                dfs(r + 1, c, area + 1),
+                dfs(r - 1, c, area + 1),
+                dfs(r, c + 1, area + 1),
+                dfs(r, c - 1, area + 1),
+            )
+
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 1 and (r, c) not in visited:
+                    islands += 1
+                    maxArea = max(
+                        maxArea,
+                        dfs(r, c, 0),
+                    )
+
+        # return islands
+        return maxArea if islands > 0 else 0
+
+
+# @lc code=end
+
+print(
+    Solution().maxAreaOfIsland(
+        [
+            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+        ]
+    )
+)
+# Output: 6
