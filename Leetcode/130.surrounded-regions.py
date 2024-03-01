f498f419b7a3f754adefa4cf70d65ea45d089a6f
Nick Wang <yw12me@gmail.com>
2024-03-01 03:56:48 -0500

    surrounded regions

---
+++ Leetcode/130.surrounded-regions.py
@@ -0,0 +1,111 @@
+#
+# @lc app=leetcode id=130 lang=python3
+#
+# [130] Surrounded Regions
+#
+# https://leetcode.com/problems/surrounded-regions/description/
+#
+# algorithms
+# Medium (38.97%)
+# Likes:    8335
+# Dislikes: 1749
+# Total Accepted:    662.8K
+# Total Submissions: 1.7M
+# Testcase Example:  '[["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]'
+#
+# Given an m x n matrix board containing 'X' and 'O', capture all regions that
+# are 4-directionally surrounded by 'X'.
+#
+# A region is captured by flipping all 'O's into 'X's in that surrounded
+# region.
+#
+#
+# Example 1:
+#
+#
+# Input: board =
+# [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+# Output:
+# [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+# Explanation: Notice that an 'O' should not be flipped if:
+# - It is on the border, or
+# - It is adjacent to an 'O' that should not be flipped.
+# The bottom 'O' is on the border, so it is not flipped.
+# The other three 'O' form a surrounded region, so they are flipped.
+#
+#
+# Example 2:
+#
+#
+# Input: board = [["X"]]
+# Output: [["X"]]
+#
+#
+#
+# Constraints:
+#
+#
+# m == board.length
+# n == board[i].length
+# 1 <= m, n <= 200
+# board[i][j] is 'X' or 'O'.
+#
+#
+#
+
+from typing import List
+
+# Key here is: board[r][c] != "O"
+# We do not want to touch anything that's not "O", meaning "T" and "X"
+# @lc code=start
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        row, col = len(board), len(board[0])
+        rowBound, colBound = range(row), range(col)
+
+        def dfs(r, c):
+            if r not in rowBound or c not in colBound or board[r][c] != "O":
+                return
+
+            board[r][c] = "T"
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in rowBound:
+            dfs(r, 0)
+            dfs(r, col - 1)
+
+        for c in colBound:
+            dfs(0, c)
+            dfs(row - 1, c)
+
+        for r in rowBound:
+            for c in colBound:
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
+
+
+# @lc code=end
+
+print(
+    Solution().solve(
+        [[["O", "O"], ["O", "O"]]]
+        # [
+        #     ["X", "X", "X", "X"],
+        #     ["X", "O", "O", "X"],
+        #     ["X", "X", "O", "X"],
+        #     ["X", "O", "X", "X"],
+        # ]
+    )
+)
+
+# Output:
+# [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
