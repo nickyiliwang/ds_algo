833ad41dc178d73b49d7a44635e759959c5dc209
Nick Wang <yw12me@gmail.com>
2024-03-24 04:56:08 -0400

    binary search WIP ones and zeroes, a knapsack dp problem

--- Leetcode/704.binary-search.py
+++ Leetcode/704.binary-search.py
@@ -54,21 +54,19 @@ from typing import *
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
+        while l < r:
+            mid = (l + r) // 2
+
+            if nums[mid] == target:
                 return mid
-        return -1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
 
-        # Input: nums = [-1,0,3,5,9,12], target = 9
-        # Output: 4
-        # Explanation: 9 exists in nums and its index is 4
+        return -1
 
 
 print(Solution.search("", [-1, 0, 3, 5, 9, 12], 9))
