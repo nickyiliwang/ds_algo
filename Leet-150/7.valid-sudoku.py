3b7cce08d7dc5338a1c84033b445cb440898853b
Nick Wang <yw12me@gmail.com>
2024-03-29 06:30:45 -0400

    valid sudoku

--- Leet-150/7.valid-sudoku.py
+++ Leet-150/7.valid-sudoku.py
@@ -1,86 +1,122 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+# https://leetcode.com/problems/valid-sudoku/description/
+#
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be
+# validated according to the following rules:
+#
+#
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9
+# without repetition.
+#
+#
+# Note:
+#
+#
+# A Sudoku board (partially filled) could be valid but is not necessarily
+# solvable.
+# Only the filled cells need to be validated according to the mentioned
+# rules.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: board =
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+#
+#
+# Example 2:
+#
+#
+# Input: board =
+# [["8","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner
+# being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it
+# is invalid.
+#
+#
+#
+# Constraints:
+#
+#
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit 1-9 or '.'.
+#
+#
+#
+from typing import List
 from collections import defaultdict
 
-
-def isValidSudoku(board):
-    # visited sets
-    rows = defaultdict(set)
-    cols = defaultdict(set)
-    squares = defaultdict(set)
-
-    # board is 9 x 9
-    for r in range(9):
-        for c in range(9):
-            if board[r][c] == ".":
-                continue
-
-            # check r,c,s
-            if (
-                board[r][c] in rows[r]
-                or board[r][c] in cols[c]
-                or board[r][c] in squares[(r // 3, c // 3)]
-            ):
-                return False
-
-            # add r,c,s into visited
-            rows[r].add(board[r][c])
-            cols[c].add(board[r][c])
-            squares[(r // 3, c // 3)].add(board[r][c])
-    return True
+# Key
+# (r // 3, c // 3) gets you the little 田 square
+# r,c,s visited sets, check and add
 
 
-# my solution after reading the trick
-# subBoxValidator uses the HashDictionary with tuple of (row/3 , and col/3) + HashSet trick
-# colValidator uses {col: HashSet}
-# rowValidator uses set in each row loop in the board
-def isValidSudoku(board):
-    subBoxValidator = defaultdict(set)
-    colValidator = defaultdict(set)
-    # {
-    #   "0": {1, 2, 3 ...}
-    #   "1": {1, 2, 3 ...}
-    #   "...9": {1, 2, 3 ...}
-    # }
-    res = True
+# @lc code=start
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # visited
+        row = defaultdict(set)
+        col = defaultdict(set)
+        sqr = defaultdict(set)
 
-    for colIdx, col in enumerate(board):
-        # we want the set to reset after each pass in row numbers
-        rowValidator = set()
-        for rowIdx, num in enumerate(col):
-            if num not in rowValidator:
-                if num != ".":
-                    rowValidator.add(num)
-            else:
-                res = False
+        for r in range(9):
+            for c in range(9):
+                curr = board[r][c]
 
-            subBoxPos = subBoxValidator[tuple([colIdx // 3, rowIdx // 3])]
-            if num not in subBoxPos:
-                if num != ".":
-                    subBoxPos.add(num)
-            else:
-                print("found dupe", subBoxPos, num)
-                res = False
+                if curr == ".":
+                    continue
 
-            key = colValidator[rowIdx]
-            if num not in key:
-                if num != ".":
-                    key.add(num)
-            else:
-                res = False
+                if curr in row[r] or curr in col[c] or curr in sqr[(r // 3, c // 3)]:
+                    return False
 
-    return res
+                row[r].add(curr)
+                col[c].add(curr)
+                sqr[(r // 3, c // 3)].add(curr)
+
+        return True
 
 
-board = [
-    [".", ".", "4", ".", ".", ".", "6", "3", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    ["5", ".", ".", ".", ".", ".", ".", "9", "."],
-    [".", ".", ".", "5", "6", ".", ".", ".", "."],
-    ["4", ".", "3", ".", ".", ".", ".", ".", "1"],
-    [".", ".", ".", "7", ".", ".", ".", ".", "."],
-    [".", ".", ".", "5", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-]
-# Output: true
+# @lc code=end
 
-isValidSudoku(board)
+print(
+    Solution().isValidSudoku(
+        [
+            ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ]
+    )
+)
