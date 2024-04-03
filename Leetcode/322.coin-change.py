8303dbe73439b4dcd4722e4c4d6bb581ec012c00
Nick Wang <yw12me@gmail.com>
2024-04-03 03:56:30 -0400

    coin change 2

--- Leetcode/322.coin-change.py
+++ Leetcode/322.coin-change.py
@@ -55,11 +55,15 @@ from typing import List
 # @lc code=start
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [0] + [float("inf")] * (amount)
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
 
         for a in range(1, amount + 1):
             for c in coins:
                 if a - c >= 0:
+                    # coin = 4
+                    # a = 7
+                    # dp[7] = 1 + dp[7 - 4]
                     dp[a] = min(dp[a], 1 + dp[a - c])
 
         return dp[amount] if dp[amount] != float("inf") else -1
--- Leetcode/518.coin-change-ii.py
+++ Leetcode/518.coin-change-ii.py
@@ -7,18 +7,18 @@
 #
 # You are given an integer array coins representing coins of different
 # denominations and an integer amount representing a total amount of money.
-# 
+#
 # Return the number of combinations that make up that amount. If that amount of
 # money cannot be made up by any combination of the coins, return 0.
-# 
+#
 # You may assume that you have an infinite number of each kind of coin.
-# 
+#
 # The answer is guaranteed to fit into a signed 32-bit integer.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: amount = 5, coins = [1,2,5]
 # Output: 4
 # Explanation: there are four ways to make up the amount:
@@ -26,39 +26,73 @@
 # 5=2+2+1
 # 5=2+1+1+1
 # 5=1+1+1+1+1
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: amount = 3, coins = [2]
 # Output: 0
 # Explanation: the amount of 3 cannot be made up just with coins of 2.
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: amount = 10, coins = [10]
 # Output: 1
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 1 <= coins.length <= 300
 # 1 <= coins[i] <= 5000
 # All the values of coins are unique.
 # 0 <= amount <= 5000
-# 
-# 
+#
+#
 #
 from typing import List
+
+
 # @lc code=start
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        
+        cache = {}
+
+        def dfs(i, a):
+            if a == amount:
+                return 1
+            if a > amount:
+                return 0
+            if i == len(coins):
+                return 0
+            if (i, a) in cache:
+                return cache[(i, a)]
+            # with a coin, and skipping a coin
+            cache[(i, a)] = dfs(i, a + coins[i]) + dfs(i + 1, a)
+            return cache[(i, a)]
+
+        return dfs(0, 0)
+
+
 # @lc code=end
 
-print(Solution().change())
+print(Solution().change(5, [1, 2, 5]))
+# 4
+
+
+# DP solution
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0] * (len(coins) + 1) for _ in range(amount + 1)]
+        dp[0] = [1] * (len(coins) + 1)
+
+        for a in range(1, amount + 1):
+            for i in range(len(coins) - 1, -1, -1):
+                dp[a][i] = dp[a][i + 1]
+                if a - coins[i] >= 0:
+                    dp[a][i] += dp[a - coins[i]][i]
+        return dp[amount][0]
