2d8b1bc32da83c369cd7f4c2700a1adef0f8a191
Nick Wang <yw12me@gmail.com>
2024-07-10 04:41:06 -0400

    reconstruct intenerary

--- 120-test/79.n-queens.py
+++ 120-test/79.n-queens.py
@@ -23,119 +23,26 @@
 # placement, where 'Q' and '.' both indicate a queen and an empty space,
 # respectively.
 #
-#
 # Example 1:
-#
-#
 # Input: n = 4
 # Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
 # Explanation: There exist two distinct solutions to the 4-queens puzzle as
 # shown above
-#
-#
+
 # Example 2:
-#
-#
 # Input: n = 1
 # Output: [["Q"]]
-#
-#
-#
+
 # Constraints:
-#
-#
 # 1 <= n <= 9
-#
-#
-#
 from typing import List
 
 
 # @lc code=start
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [["." for _ in range(n)] for _ in range(n)]
-        res = []
-        colPath = set()
-        downRightDiagonal = set()  # (r - c)
-        upRightDiagonal = set()  # (r + c)
-
-        def backtrack(r):
-            if r == n:
-                formatted = ["".join(row) for row in board]
-                res.append(formatted)
-                return
-
-            for c in range(n):
-                if (
-                    c in colPath
-                    or (r + c) in upRightDiagonal
-                    or (r - c) in downRightDiagonal
-                ):
-                    continue
-
-                colPath.add(c)
-                downRightDiagonal.add((r - c))
-                upRightDiagonal.add((r + c))
-                board[r][c] = "Q"
-                backtrack(r + 1)
-                colPath.remove(c)
-                downRightDiagonal.remove((r - c))
-                upRightDiagonal.remove((r + c))
-                board[r][c] = "."
-
-        backtrack(0)
-        return res
-
 
 # @lc code=end
 
 print(Solution().solveNQueens(4))
-# Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
-
-# WIP my solution
-# class Solution:
-#     def solveNQueens(self, n: int) -> List[List[str]]:
-#         row, col = n, n
-#         # board = [["." for _ in range(n)] for _ in range(n)]
-#         board = [["." for _ in range(n)] for _ in range(n)]
-#         # board = ["." * n for _ in range(n)]
-#         res = []
-#         colPath = set()
-#         downRightDiagonal = set()  # (r - c)
-#         upRightDiagonal = set()  # (r + c)
-
-#         def dfs(i, r, c, queens, board):
-#             if queens == n:
-#                 res.append(board)
-#                 return
-
-#             if (
-#                 r < 0
-#                 or r >= row
-#                 or c < 0
-#                 or c >= col
-#                 or c in colPath
-#                 or board[r][c] == "Q"
-#             ):
-#                 return
-
-#             board[r][c] = "Q"
-#             queens += 1
-
-#             colPath.add(col)
-#             downRightDiagonal.add((r - c))
-#             upRightDiagonal.add((r + c))
-#             dfs(i + 1, r + 1, c, queens, board)
-#             dfs(i + 1, r - 1, c, queens, board)
-#             dfs(i + 1, r, c + 1, queens, board)
-#             dfs(i + 1, r, c - 1, queens, board)
-#             colPath.remove(col)
-#             downRightDiagonal.remove((r - c))
-#             upRightDiagonal.remove((r + c))
-
-#         for r in range(row):
-#             for c in range(col):
-#                 dfs(0, r, c, 0, board)
-
-#         return res
+# [[".Q..", "...Q", "Q...", "..Q."], ["..Q.", "Q...", "...Q", ".Q.."]]
--- Fundamentals/Misc/sandbox-playground.py
+++ Fundamentals/Misc/sandbox-playground.py
@@ -1,17 +1,59 @@
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float("inf")] * (amount + 1)
-        dp[0] = 0
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        stack = ["JFK"]
+        res = ["JFK"]
 
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - c])
+        for src, dst in tickets:
+            adj[src].append(dst)
+            # adj[dst].append(src)
 
-            return dp[amount] if dp[amount] != float("inf") else -1
+        while stack:
+            start = stack.pop()
+            tmp = []
+            for d in adj[start]:
+                weight = 0
+                for s in d:
+                    weight += ord(s)
+
+                if tmp:
+                    if tmp[0] < weight:
+                        tmp = [weight, d]
+                else:
+                    tmp = [weight, d]
+
+            if tmp:
+                stack.append(tmp[1])
+                res.append(tmp[1])
+                adj[start].remove(tmp[1])
+
+        return res
+
+
+print(
+    Solution().findItinerary(
+        [["JFK", "SFO"], ["JFK", "ATL"], ["SFO", "ATL"], ["ATL", "JFK"], ["ATL", "SFO"]]
+        # [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+    )
+)
+
+# from typing import List
+
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         dp = [float("inf")] * (amount + 1)
+#         dp[0] = 0
+
+#         for a in range(1, amount + 1):
+#             for c in coins:
+#                 if a - c >= 0:
+#                     dp[a] = min(dp[a], 1 + dp[a - c])
+
+#             return dp[amount] if dp[amount] != float("inf") else -1
 
 
 # node = None
